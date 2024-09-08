f412bc67f146e4fd25df77666c01b4087d12495e
ChoongHo <ch.neizzz@gmail.com>
2024-09-08 15:32:38 +0900

    feat: fe env + env typing

--- packages/apps/frontend/vite.config.ts
+++ packages/apps/frontend/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    __NVIDEO_API_URL__: JSON.stringify(process.env.NVIDEO_API_URL),
+  },
+});
