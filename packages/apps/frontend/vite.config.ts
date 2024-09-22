79fa16004a9a3db152f891d3c7e7f8499013c4b7
ChoongHo <ch.neizzz@gmail.com>
2024-09-23 00:11:00 +0900

    fix: modify nginx conf for fe asset path

--- packages/apps/frontend/vite.config.ts
+++ packages/apps/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nvideo-dev/',
   plugins: [react()],
   define: {
     __NVIDEO_API_URL__: JSON.stringify(process.env.NVIDEO_API_URL),
